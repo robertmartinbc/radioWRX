99eb09c756757e398f0a5eaf8c1a21ecdbc5e6fb
Robert Martin <robertmartinbc@shaw.ca>
2017-04-08 09:23:43 -0700

    Added in Bands Creat Album Page.

--- src/pages/bands-view-bands-profile/bands-view-bands-profile.ts
+++ src/pages/bands-view-bands-profile/bands-view-bands-profile.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { RegisterBandsAlbumDetailsPage } from '../register-bands-album-details/register-bands-album-details';
+import { RegisterBandsMembersDetailsPage } from '../register-bands-members-details/register-bands-members-details';
+
 
 /*
   Generated class for the BandsViewBandsProfile page.
@@ -17,6 +19,7 @@ export class BandsViewBandsProfilePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   registerBandsAlbumDetails = RegisterBandsAlbumDetailsPage;
+  registerBandsMembersDetails = RegisterBandsMembersDetailsPage;
   ionViewDidLoad() {
     console.log('ionViewDidLoad BandsViewBandsProfilePage');
   }
