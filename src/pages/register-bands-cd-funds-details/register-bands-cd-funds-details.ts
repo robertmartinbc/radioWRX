a854f941cb89eaf6aecca19d70304d232cfa29d4
Robert Martin <robertmartinbc@shaw.ca>
2017-04-27 21:10:10 -0700

    Added CD Funds and Private Party creation pages.

--- src/pages/register-bands-cd-funds-details/register-bands-cd-funds-details.ts
+++ src/pages/register-bands-cd-funds-details/register-bands-cd-funds-details.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as firebase from 'firebase';
+import { BandsViewBandsProfilePage } from '../bands-view-bands-profile/bands-view-bands-profile';
+import { AngularFire } from 'angularfire2';
+
+//Create class for a new CD Funds Request
+class CDFunds {
+  targetDate: string
+  targetAmount: string
+}
 
 /*
   Generated class for the RegisterBandsCDFundsDetails page.
@@ -12,9 +21,17 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register-bands-cd-funds-details.html'
 })
 export class RegisterBandsCDFundsDetailsPage {
+  cdfunds: CDFunds = new CDFunds()
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {}
 
+  submit() {
+    //this.cdfunds.userId = firebase.auth().currentUser.uid;
+    this.af.database.list('/cdfunds').push(this.cdfunds);
+    this.cdfunds = new CDFunds();
+    this.navCtrl.pop(BandsViewBandsProfilePage)
+  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterBandsCDFundsDetailsPage');
   }
--- src/pages/register-bands-private-party-details/register-bands-private-party-details.ts
+++ src/pages/register-bands-private-party-details/register-bands-private-party-details.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { BandsViewBandsProfilePage } from '../bands-view-bands-profile/bands-view-bands-profile';
+import { AngularFire } from 'angularfire2';
+
+class PrivateParty {
+  zipcode: string
+  fee: string
+}
 
 /*
   Generated class for the RegisterBandsPrivatePartyDetails page.
@@ -12,8 +19,15 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register-bands-private-party-details.html'
 })
 export class RegisterBandsPrivatePartyDetailsPage {
+  privateparty: PrivateParty = new PrivateParty()
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  submit() {
+    //this.privateparty.userId = firebase.auth().currentUser.uid;
+    this.af.database.list('/privateparty').push(this.privateparty);
+    this.privateparty = new PrivateParty();
+    this.navCtrl.pop(BandsViewBandsProfilePage);
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterBandsPrivatePartyDetailsPage');
