1ba2ceab0a196cf13b324df8ea451cad977a1a65
Robert Martin <robertmartinbc@shaw.ca>
2017-05-06 01:23:56 -0700

    Repositioned Sing in button and added to additional pages. Further pages still required, though.

--- src/pages/tickets/tickets.ts
+++ src/pages/tickets/tickets.ts
@@ -1,22 +1,56 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
 import { FansViewTicketsPage } from '../fans-view-tickets/fans-view-tickets';
+import { SignInModalPage } from '../sign-in-modal/sign-in-modal';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import * as firebase from 'firebase';
 
-
-/*
-  Generated class for the Tickets page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-tickets',
   templateUrl: 'tickets.html'
 })
 export class TicketsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  isLoggedIn: boolean = false;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  public modalCtrl: ModalController, public viewCtrl: ViewController,
+  public af: AngularFire) {
+
+    var _self = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user)
+        _self.isLoggedIn=true;
+      else
+        _self.isLoggedIn=false;
+    });
+  }
+
+  //Present Sign Modal for RadioWRX User
+  presentSignInModal() {
+    var _self = this;
+    let modal = this.modalCtrl.create(SignInModalPage);
+    modal.present();
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user)
+        _self.isLoggedIn=true;
+      else
+        _self.isLoggedIn=false;
+    });
+  }
+
   fansViewTickets = FansViewTicketsPage;
+
+  signOut(){
+    // FIXME: Consider when somewrong occur with signOut by firebase
+    this.isLoggedIn=false;
+    firebase.auth().signOut().then(function() {
+      alert('Sign out success.');
+    }, function(error) {
+      alert('Error to sign out.');
+    });
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad TicketsPage');
   }
